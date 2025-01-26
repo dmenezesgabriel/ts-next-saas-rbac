c70118852000a14d0cb258567846e607bd168c6b
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-01-26 17:57:36 -0300

    feat: add permissions

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,9 @@
 import { defineAbilityFor } from "@saas/auth";
 
-const ability = defineAbilityFor({ role: "MEMBER" });
-
-const userCanInviteSomeoneElse = ability.can("invite", "User");
+const ability = defineAbilityFor({ role: "MEMBER", id: "user-id" });
 const userCanDeleteOtherUsers = ability.can("delete", "User");
 
 const userCannotDeleteOtherUsers = ability.cannot("delete", "User");
 
 console.log(userCannotDeleteOtherUsers);
-console.log(userCanInviteSomeoneElse);
 console.log(userCanDeleteOtherUsers);
