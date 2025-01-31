5a44baef23eedcfc54b65701ea2315785cf79fac
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-01-30 23:15:04 -0300

    chore: add swagger

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,12 +1,14 @@
 import fastifyCors from "@fastify/cors";
 import { fastify } from "fastify";
+import fastifySwagger from "@fastify/swagger";
 import {
-  // jsonSchemaTransform,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import { createAccount } from "./routes/auth/create-account";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 const loggerConfig = {
   development: {
@@ -23,6 +25,20 @@ const app = fastify({
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Next.js SaaS",
+      description: "Full-stack SaaS app with multi-tenant & RBAC",
+      version: "1.0.0",
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, { routePrefix: "/docs" });
 
 app.register(fastifyCors);
 app.register(createAccount);
