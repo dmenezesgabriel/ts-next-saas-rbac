8f888ebdf7ad7bd52b2ce350b989de8d2bf2ebd2
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-02-02 15:09:01 -0300

    feat: add organization routes

--- apps/api/src/http/middlewares/auth.ts
+++ apps/api/src/http/middlewares/auth.ts
@@ -1,6 +1,7 @@
 import type { FastifyInstance } from "fastify";
 import { UnauthorizedError } from "../routes/_error/unauthorized-error";
 import fastifyPlugin from "fastify-plugin";
+import { prisma } from "@/lib/prisma";
 
 export const auth = fastifyPlugin(async (app: FastifyInstance) => {
   app.addHook("preHandler", async (request) => {
@@ -12,6 +13,34 @@ export const auth = fastifyPlugin(async (app: FastifyInstance) => {
       } catch {
         throw new UnauthorizedError("Invalid auth token");
       }
+    };
+
+    request.getUserMembership = async (slug: string) => {
+      const userId = await request.getCurrentUserId();
+      const member = await prisma.member.findFirst({
+        where: {
+          userId,
+          organization: {
+            slug,
+          },
+        },
+        include: {
+          organization: true,
+        },
+      });
+
+      if (!member) {
+        throw new UnauthorizedError(
+          "You're not a member of this organization."
+        );
+      }
+
+      const { organization, ...membership } = member;
+
+      return {
+        organization,
+        membership,
+      };
     };
   });
 });
