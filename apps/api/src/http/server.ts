e1ae13d7332a2a2b0778deb4ec44ab51841f947c
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-02-02 11:51:24 -0300

    feat: add create organization route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -17,6 +17,7 @@ import { requestPasswordRecover } from "./routes/auth/request-password-recover";
 import { resetPassword } from "./routes/auth/reset-password";
 import { authenticateWithGithub } from "./routes/auth/authenticate-with-github";
 import { env } from "@saas/env";
+import { createOrganization } from "./routes/orgs/create-organization";
 
 const loggerConfig = {
   development: {
@@ -68,6 +69,7 @@ app.register(getProfile);
 app.register(requestPasswordRecover);
 app.register(resetPassword);
 app.register(authenticateWithGithub);
+app.register(createOrganization);
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   app.log.info("🚀 HTTP server running!");
